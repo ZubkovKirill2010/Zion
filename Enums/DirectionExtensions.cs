23f4783bcad27310e3002736c4defbc971f1d276
ZubkovKirill2010 <zubkovkirill2010@mail.ru>
2025-08-18 21:20:26 +0300

    Вернул удалённую логику DirectionExtensions

--- Enums/DirectionExtensions.cs
+++ Enums/DirectionExtensions.cs
@@ -4,9 +4,14 @@ namespace Zion
 {
     public static class DirectionExtensions
     {
-        /// <summary>
-        /// Converts direction to absolute value (Left->Right, Down->Up).
-        /// </summary>
+        private static readonly Direction[] Directions =
+        [
+            Direction.Up,
+            Direction.Right,
+            Direction.Down,
+            Direction.Left
+        ];
+
         public static Direction Abs(this Direction Direction)
         {
             switch (Direction)
@@ -17,9 +22,18 @@ namespace Zion
             return Direction;
         }
 
-        /// <summary>
-        /// Converts direction to 2D vector representation.
-        /// </summary>
+        public static Direction GetByIndex(int Index)
+        {
+            return Directions[Index];
+        }
+        public static Direction GetByIndex(this Direction Direction, int Index)
+        {
+            return GetByIndex(Index);
+        }
+
+        public static Direction[] GetDirections() => Directions;
+        public static Direction[] GetDirections(this Direction Direction) => Directions;
+
         public static Vector2 ToVector2(this Direction Direction)
         {
             switch (Direction)
@@ -31,7 +45,16 @@ namespace Zion
             }
             return Vector2.Zero;
         }
-
-        // Other methods remain the same...
+        public static Vector2Int ToVector2Int(this Direction Direction)
+        {
+            switch (Direction)
+            {
+                case Direction.Up: return Vector2Int.Up;
+                case Direction.Down: return Vector2Int.Down;
+                case Direction.Left: return Vector2Int.Left;
+                case Direction.Right: return Vector2Int.Right;
+            }
+            return Vector2Int.Zero;
+        }
     }
 }
