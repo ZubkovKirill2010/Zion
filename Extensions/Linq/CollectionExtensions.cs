df04bd20423679912c34842355f4a71641742587
zubkovkirill2010 <zubkovkirill2010@mail.ru>
2025-08-25 13:16:24 +0300

    _

--- Extensions/Linq/CollectionExtensions.cs
+++ Extensions/Linq/CollectionExtensions.cs
@@ -6,5 +6,36 @@
         {
             return Collection is null || Collection.Count == 0;
         }
+
+        public static TOut[] ToArray<TIn, TOut>(this ICollection<TIn> Collection, Converter<TIn, TOut> Converter)
+        {
+            ArgumentNullException.ThrowIfNull(Collection);
+            ArgumentNullException.ThrowIfNull(Converter);
+
+            TOut[] Result = new TOut[Collection.Count];
+            int Index = 0;
+
+            foreach (TIn Item in Collection)
+            {
+                Result[Index++] = Converter(Item);
+            }
+
+            return Result;
+        }
+
+        public static List<TOut> ToList<TIn, TOut>(this ICollection<TIn> Collection, Converter<TIn, TOut> Converter, int AdditionalCapacity = 0)
+        {
+            ArgumentNullException.ThrowIfNull(Collection);
+            ArgumentNullException.ThrowIfNull(Converter);
+
+            List<TOut> Result = new List<TOut>(Collection.Count + Math.Max(0, AdditionalCapacity));
+
+            foreach (TIn Item in Collection)
+            {
+                Result.Add(Converter(Item));
+            }
+
+            return Result;
+        }
     }
 }
