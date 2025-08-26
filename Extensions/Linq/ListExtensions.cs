9807baa5d0f4a3c8e8f695cd87551175c1b9cdca
zubkovkirill2010 <zubkovkirill2010@mail.ru>
2025-08-26 20:42:40 +0300

    Добавил метод Oscilate

--- Extensions/Linq/ListExtensions.cs
+++ Extensions/Linq/ListExtensions.cs
@@ -26,6 +26,50 @@
             return Result;
         }
 
+        public static T[] ToReversedArray<T>(this IList<T> List)
+        {
+            T[] Result = new T[List.Count];
+
+            int Start = 0;
+            int End = List.Count - 1;
+
+            while (End >= 0)
+            {
+                Result[Start++] = List[End--];
+            }
+            return Result;
+        }
+
+        /// <summary>
+        /// Generates a sequence that goes forward through the collection and then backward.
+        /// For example: [1, 2, 3] -> 1, 2, 3, 2, 1
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the collection</typeparam>
+        /// <param name="List">The source collection</param>
+        /// <returns>An oscillating sequence of elements</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the input collection is null</exception>
+        public static IEnumerable<T> Oscillate<T>(this IList<T> List)
+        {
+            ArgumentNullException.ThrowIfNull(List);
+
+            if (List.Count == 0)
+            {
+                yield break;
+            }
+
+            int End = List.Count - 1;
+
+            for (int i = 0; i < End; i++)
+            {
+                yield return List[i];
+            }
+            for (int j = End; j >= 0; j--)
+            {
+                yield return List[j];
+            }
+        }
+
+
         public static void RemoveAt<T>(this List<T> List, params int[] Indexes)
         {
             if (Indexes.Length == 0 || List.Count == 0)
@@ -68,20 +112,6 @@
             }
 
             List.RemoveRange(WritePosition, List.Count - WritePosition);
-        }
-
-        public static T[] ToReversedArray<T>(this IList<T> List)
-        {
-            T[] Result = new T[List.Count];
-
-            int Start = 0;
-            int End = List.Count - 1;
-
-            while (End >= 0)
-            {
-                Result[Start++] = List[End--];
-            }
-            return Result;
         }
     }
 }
