5e125e9503cd020882ef78744b56999b82cca735
ZubkovKirill2010 <zubkovkirill2010@mail.ru>
2026-05-21 21:14:31 +0300

    Single tokens
    Добавил токены для одиночных символов.

---
+++ STP/Tokens/Tokens/Single/BracketTokens.cs
@@ -0,0 +1,11 @@
+﻿namespace Zion.STP
+{
+    public sealed class OpenBracketToken : Token { }
+    public sealed class CloseBracketToken : Token { }
+
+    public sealed class OpenSquareBracketToken : Token { }
+    public sealed class CloseSquareBracketToken : Token { }
+
+    public sealed class OpenCurlyBracketToken : Token { }
+    public sealed class CloseCurlyBracketToken : Token { }
+}
---
+++ STP/Tokens/Tokens/Single/SingleTokens.cs
@@ -0,0 +1,47 @@
+﻿namespace Zion.STP
+{
+    //.
+    public sealed class DotToken   : Token { }
+
+    //,
+    public sealed class CommaToken : Token { }
+
+    //;
+    public sealed class SemicolonToken : Token { }
+
+    //:
+    public sealed class ColonToken : Token { }
+
+    //+
+    public sealed class PlusToken : Token { }
+
+    //-
+    public sealed class MinusToken : Token { }
+
+    //*
+    public sealed class AsteriskToken : Token { }
+
+    //'/'
+    public sealed class SlashToken : Token { }
+
+    //=
+    public sealed class EqualsToken : Token { }
+
+    //!
+    public sealed class ExclamationMarkToken : Token { }
+
+    //&
+    public sealed class AmpersandToken : Token { }
+
+    //|
+    public sealed class PipeToken : Token { }
+
+    //~
+    public sealed class TildeToken : Token { }
+
+    //^
+    public sealed class CaretToken : Token { }
+
+    //?
+    public sealed class QuestionMarkToken : Token { }
+}
