7fb6c3c567cca5bc2b0591ae1fdead91c45dbea6
ZubkovKirill2010 <zubkovkirill2010@mail.ru>
2026-06-07 20:19:51 +0300

    Create SingleTokenNodeReader.cs
    Сделал SingleTokenNodeReader для нод длиной в 1 токен

---
+++ STP/Nodes/Readers/SingleTokenNodeReader.cs
@@ -0,0 +1,30 @@
+﻿namespace Zion.STP
+{
+    public sealed class SingleTokenNodeReader<Node> : INodeReader<Node> where Node : STP.Node
+    {
+        private readonly ITokenMatcher TokenMatcher;
+        private readonly Func<Token, Node> NodeGetter;
+
+        public SingleTokenNodeReader(ITokenMatcher TokenMatcher, Func<Token, Node> NodeGetter)
+        {
+            this.TokenMatcher = TokenMatcher.NotNull();
+            this.NodeGetter = NodeGetter.NotNull();
+        }
+
+        public bool Read(TokenSlice Tokens, out Node Node)
+        {
+            Token Token = Tokens[0];
+
+            if (TokenMatcher.Match(Token, out bool GoToNext))
+            {
+                ArgumentException.ThrowIf(!GoToNext, "SingleTokenNodeReader requires GoToNext = true. The token matched but refused to advance.");
+
+                Node = NodeGetter(Token);
+                return true;
+            }
+
+            Node = default!;
+            return false;
+        }
+    }
+}
