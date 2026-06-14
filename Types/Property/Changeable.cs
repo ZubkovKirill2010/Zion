ad2c9e74291ff72c5b25ae8218a0a0d039fd6520
ZubkovKirill2010 <zubkovkirill2010@mail.ru>
2026-06-14 23:03:30 +0300

    Добавил Changeable

---
+++ Types/Property/Changeable.cs
@@ -0,0 +1,34 @@
+﻿namespace Zion
+{
+    public sealed class Changeable<T>
+    {
+        public static readonly Func<T, T, bool> NotNull = static (Old, New) => New is not null;
+
+        public T Value
+        {
+            get;
+            set
+            {
+                if (CanSet(field, value))
+                {
+                    field = value;
+                    Changed?.Invoke(value);
+                }
+            }
+        }
+
+        private readonly Func<T, T, bool> CanSet = static (Old, New) => true;
+
+        public Changeable(T DefaultValue)
+        {
+            Value = DefaultValue;
+        }
+        public Changeable(T DefaultValue, Func<T, T, bool> CanSet)
+        {
+            this.CanSet ??= CanSet;
+            Value = DefaultValue;
+        }
+
+        public event Action<T>? Changed;
+    }
+}
