2f7b974f11eb9d74775a51c5d36e7ab299949b07
ZubkovKirill2010 <zubkovkirill2010@mail.ru>
2026-06-15 13:00:18 +0300

    Token comparing
    Добавил методы для сравнения токенов.

--- STP/Tokens/Base/Token.cs
+++ STP/Tokens/Base/Token.cs
@@ -1,6 +1,8 @@
-﻿namespace Zion.STP
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Zion.STP
 {
-    public abstract class Token
+    public abstract class Token : IEqualityComparer<Token>
     {
         #region Constants
         private static readonly short[] EmptyErrorCodes = Array.Empty<short>();
@@ -25,7 +27,8 @@
             get;
             set
             {
-                field = value.NotNull();
+                value ??= EmptyErrorCodes;
+                field = value;
                 Status = value.Length == 0 ? Validation.Valid : Validation.Invalid;
             }
         } = EmptyErrorCodes;
@@ -66,6 +69,22 @@
             return $"[{GetType().Name.RemoveSuffix("Token")}]";
         }
 
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(GetType(), Length);
+        }
+
+        public override bool Equals(object? Object)
+        {
+            return Object is Token Token && Equals(this, Token);
+        }
+
+
+        public int GetHashCode([DisallowNull] Token Object)
+        {
+            return Object.GetHashCode();
+        }
+
 
         public void AddError(short ErrorCode)
         {
@@ -75,6 +94,27 @@
         public void ClearErrors()
         {
             ErrorCodes = EmptyErrorCodes;
+        }
+
+        #endregion
+
+        #region EqualityComparing
+        public bool Equals(Token? A, Token? B)
+        {
+            if (ReferenceEquals(A, B))  { return true;  }
+            if (A is null || B is null) { return false; }
+
+            return A.GetType() == B.GetType() && A.Length == B.Length && A.DataEquals(B);
+        }
+
+        public bool Equals(Token? Other)
+        {
+            return Equals(this, Other);
+        }
+
+        protected virtual bool DataEquals(Token Other)
+        {
+            return true;
         }
 
         #endregion
