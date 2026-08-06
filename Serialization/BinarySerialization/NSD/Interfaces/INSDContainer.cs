d24497689f99f96b58660825543a29dfab3db2c9
ZubkovKirill2010 <zubkovkirill2010@mail.ru>
2026-08-06 14:04:53 +0300

    Clearing
    Решил полностью переделать формат NSD

--- Serialization/BinarySerialization/NSD/Interfaces/INSDContainer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Zion.Serialization.NSD
-{
-    public interface INSDContainer<T> where T : INSDContainer<T>
-    {
-        public void Write(NSDWriteContext Context);
-
-        public static abstract T Read(NSDReadContext Context);
-    }
-}
